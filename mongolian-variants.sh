a2da291ab0b0c5487daf2a485f2d8d6d0fce1d11
Hin-Tak Leung <htl10@users.sourceforge.net>
2018-10-26 00:12:16 +0100

    new image generaton script

---
+++ mongolian-variants.sh
@@ -0,0 +1,10 @@
+./hb-view.py ${1}.ttf 'ᠴᠢᠩᠭᠢᠰ ᠬᠠᠭᠠᠨ'
+mv hb-view.png ${1}-bare.png
+./hb-view.py ${1}.ttf 'ᠴ ᠢ ᠩ ᠭ ᠢ ᠰ ᠬ ᠠ ᠭ ᠠ ᠨ'
+mv hb-view.png ${1}-form0.png
+./hb-view.py ${1}.ttf 'ᠴ᠋ ᠢ᠋ ᠩ᠋ ᠭ᠋ ᠢ᠋ ᠰ᠋ ᠬ᠋ ᠠ᠋ ᠭ᠋ ᠠ᠋ ᠨ᠋'
+mv hb-view.png ${1}-form1.png
+./hb-view.py ${1}.ttf 'ᠴ᠌ ᠢ᠌ ᠩ᠌ ᠭ᠌ ᠢ᠌ ᠰ᠌ ᠬ᠌ ᠠ᠌ ᠭ᠌ ᠠ᠌ ᠨ᠌'
+mv hb-view.png ${1}-form2.png
+./hb-view.py ${1}.ttf 'ᠴ᠍ ᠢ᠍ ᠩ᠍ ᠭ᠍ ᠢ᠍ ᠰ᠍ ᠬ᠍ ᠠ᠍ ᠭ᠍ ᠠ᠍ ᠨ᠍'
+mv hb-view.png ${1}-form3.png
